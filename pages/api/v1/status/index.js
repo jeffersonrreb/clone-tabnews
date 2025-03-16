81394a12c6fc48f1928829a67fa11ff3eca9b186
Jefferson Rebouças <48016639+jeffersonrreb@users.noreply.github.com>
2025-03-16 01:06:11 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,11 @@
-function status(request, response) {
-  response.status(200).json({
-    message: "Num é que estou aprendendo com o curso.dev! 🤩",
-  });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response
+    .status(200)
+    .json({ message: "Num é que estou aprendendo com o curso.dev! 🤩" });
 }
 
 export default status;
